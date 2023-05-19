9d096688f7bc9af24f13aa1fb436612733ba1f28
YisrealHung <43162482+YisrealHung@users.noreply.github.com>
2023-05-19 14:28:56 +0800

    Update main.ts
    change connect function

--- main.ts
+++ main.ts
@@ -203,7 +203,8 @@ namespace logyun {
             if (readWifiState == "ok") {
                 break;
             }
-            basic.pause(500);
+            basic.pause(100);
+            /*
             clear_ = serial.readString();
             serial.writeLine("WifiCheck()");
             readWifiState = serial.readLine()
@@ -211,6 +212,7 @@ namespace logyun {
                 break;
             }
             basic.pause(500);
+            */
         }
 
     }
@@ -232,7 +234,8 @@ namespace logyun {
             if (readWifiState == "ok") {
                 break;
             }
-            basic.pause(500);
+            basic.pause(100);
+            /*
             clear_ = serial.readString();
             serial.writeLine("WifiCheck()");
             readWifiState = serial.readLine()
@@ -240,6 +243,7 @@ namespace logyun {
                 break;
             }
             basic.pause(500);
+            */
         }
     }
 
